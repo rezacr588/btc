56fd0c8bba8b6e751d6f02305c265d0b40a701d4
rzeraat.tur <rzeraat.tur@gmail.com>
2023-09-19 11:31:51 +0300

    init

---
+++ src/data/data.py
@@ -0,0 +1,72 @@
+import os
+import requests
+import pandas as pd
+
+save_raw_path = 'data/raw/bitcoin_data.csv'
+
+def download_bitcoin_data(save_path=save_raw_path):
+    """
+    Downloads Bitcoin data from a specified URL and saves it to a given path.
+    
+    Parameters:
+    - save_path (str): Path where the CSV data should be saved. Default is 'data/bitcoin_data.csv'.
+    
+    Returns:
+    - str: Path where the data was saved.
+    """
+    # URL of the CSV data
+    url = "https://bitcoin-data-collective.vercel.app/api/download_btc"
+
+    # Make a request to the URL
+    response = requests.get(url)
+
+    # Ensure the request was successful
+    response.raise_for_status()
+
+    # Create directory if it doesn't exist
+    directory = os.path.dirname(save_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Write the content to a CSV file
+    with open(save_path, 'wb') as file:
+        file.write(response.content)
+
+    print(f"Data saved to {save_path}")
+    return save_path
+
+def preprocess_bitcoin_data(file_path=save_raw_path):
+    """
+    Preprocesses the Bitcoin data.
+    
+    Parameters:
+    - file_path (str): Path to the CSV data file.
+    
+    Returns:
+    - DataFrame: Preprocessed data.
+    """
+    # Load the data
+    df = pd.read_csv(file_path)
+    
+    # Convert the 'TIME' column to datetime format
+    df['TIME'] = pd.to_datetime(df['TIME'], unit='s')
+    
+    # Check if the data is ordered chronologically
+    if not df['TIME'].is_monotonic:
+        df.sort_values(by='TIME', inplace=True)
+        print("Data was not ordered chronologically. It has been sorted.")
+    else:
+        print("Data is ordered chronologically.")
+    
+    # Handle missing values (for this example, we'll drop them)
+    df.dropna(inplace=True)
+    
+    # Create lag features for 'LAST_PRICE' and 'VOLUME_24H'
+    for i in range(1, 61):  # for 60 minutes
+        df[f'LAST_PRICE_LAG_{i}'] = df['LAST_PRICE'].shift(i)
+        df[f'VOLUME_24H_LAG_{i}'] = df['VOLUME_24H'].shift(i)
+    
+    # Drop rows with NaN values caused by the lag features
+    df.dropna(inplace=True)
+    
+    return df
---
+++ src/models/model.py
@@ -0,0 +1,62 @@
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+def time_based_split(data, test_size=0.2):
+    """
+    Splits the data into training and testing sets based on the given test size.
+    
+    Parameters:
+    - data (DataFrame): The dataset to be split.
+    - test_size (float): Proportion of the dataset to include in the test split (e.g., 0.2 for 20%).
+    
+    Returns:
+    - DataFrame, DataFrame: Training and testing datasets.
+    """
+    # Calculate the index at which to split the data
+    split_idx = int(len(data) * (1 - test_size))
+    
+    # Split the data
+    train_data = data.iloc[:split_idx]
+    test_data = data.iloc[split_idx:]
+    
+    return train_data, test_data
+
+def prepare_data(train_data, test_data):
+    """Separate features and target from training and testing data."""
+    X_train = train_data.drop(columns=['LAST_PRICE', 'TIME'])
+    y_train = train_data['LAST_PRICE']
+
+    X_test = test_data.drop(columns=['LAST_PRICE', 'TIME'])
+    y_test = test_data['LAST_PRICE']
+    
+    return X_train, y_train, X_test, y_test
+
+def train_xgboost(X_train, y_train, params=None, num_rounds=100):
+    """Train an XGBoost model."""
+    if params is None:
+        params = {
+            'objective': 'reg:squarederror',
+            'eval_metric': 'rmse',
+            'booster': 'gbtree',
+            'verbosity': 1
+        }
+
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+    bst = xgb.train(params, dtrain, num_rounds)
+    
+    return bst
+
+def evaluate_model(bst, X_test, y_test):
+    """Evaluate the XGBoost model and return RMSE."""
+    dtest = xgb.DMatrix(X_test)
+    y_pred = bst.predict(dtest)
+    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    
+    return rmse
+
+
+def plot_feature_importance(bst):
+    """Plot feature importance for the XGBoost model."""
+    xgb.plot_importance(bst)
+    plt.show()
